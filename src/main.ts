0952b29efe5278ce8b073950c1b09c32c8e6ec7c
Eric Dourado <ericdourado1@gmail.com>
2025-03-05 16:17:09 -0300

    fix: enable cors for all origins

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true}));
+  app.enableCors({
+    origin: '*'
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Nex Community API')
